3daba96aba08d94b224744d0d6385b6efd68b9c0
Nice <puma1800@naver.com>
2023-01-31 23:09:29 +0900

    연습

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import './App.css';
-import Nav from 'react-bootstrap/Nav';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import bg from './img/bg.png'
 import data from './data';
 import { useState } from 'react';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail';
 
 function App() {
 
-  const [shoes] = useState(data)
+  const [shoes] = useState(data);
+  const Navigate = useNavigate();
 
   return (
     <>
-      <Nav
-      activeKey="/home"
-      onSelect={(e) => alert(`selected ${e}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/home">쇼핑몰</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-    </Nav>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="/" style={{marginLeft:'20px'}}>쇼핑몰</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link onClick={()=>(Navigate('/'))}>홈</Nav.Link>
+          <Nav.Link onClick={()=>(Navigate('/detail'))}>상세페이지</Nav.Link>
+          <Nav.Link onClick={()=>(Navigate('/item'))}>아이템</Nav.Link>
+        </Nav>
+
+      </Navbar>
     <div className='main-bg' style={{backgroundImage : 'url('+bg+')'}}></div>
 
     
+    
+    <Routes>
+      <Route path='/' element={
     <div className='container'>
     {shoes.map((shoes, i)=>(
-      <Card shoes={shoes} i={i+1}/>))}
+    <Card shoes={shoes} i={i+1}/>))}
     </div>
+    }></Route>
+      <Route path='/detail' element={<Detail/>}></Route>
+      <Route path='/item' element={<p>아이템이에요</p>}></Route>
+    </Routes>
     
     </>
   );
---
+++ src/routes/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Detail() {
+    return (
+        <div>디테일페이지입니다!!!!!</div>
+    )
+}
+
+export default Detail
